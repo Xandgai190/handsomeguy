810ffd145752d61d2ef9bc37a4f86dd9dc18b4db
Xandgai190 <chinboldganerdene@gmail.com>
2023-07-26 20:52:45 -0700

    011_listp

---
+++ 011_listp.py
@@ -0,0 +1,41 @@
+##scores=[90,80,70,60,50]
+##print(scores)
+##average=sum(scores)/5
+##print(average)
+
+##a=list(range(1,11,1))
+##print(a)
+##b=list(range(5,50,5))
+##print(b)
+##c=list(range(8,80,8))
+##print(c)
+
+
+##a=[1,2,3,4,5]
+##print(a)
+##a.insert(1,10)
+##print(a)
+##a.insert(2,"Hello")
+##print(a)
+
+##a=[1,2,3]
+##b=[4,5,6]
+##a.extend(b)
+##print(a)
+##print(b)
+##
+##a += [1,2,3]
+##b -= [1,2,3]
+##c *= [1,2,3]
+##print(a)
+
+
+a=[1,2,3,4,5]
+i=0
+
+while i<=4:
+ print(a[i])
+i += 1
+print("\n")
+for n in a:
+      print(n)
---
+++ listp.py
@@ -0,0 +1,36 @@
+##scores=[]
+##SUM=0
+##for i in range(5):
+##     scores.append(int(input("score input %d: " %(i+1))))
+##     
+##print(scores)
+
+##
+##a=[10,20,20,40,50]
+##print(a.count(10))
+##print(a.count(20))
+
+##a=[10,20,30,40,50]
+##print(a)
+##a.remove(10)
+##print(a)
+
+##
+##a=[10,20,30,40,50]
+##print(a.index(30))
+##
+##a=[20,40,10,90,80]
+##a.sort()
+##print(a)
+
+##
+##a=[10,20,30,40,50]
+##print(a.pop())
+##print(a.pop())
+##print(a.pop(0))
+
+
+for i in range(2,10):
+    for j in range(1,10):
+        print(i*j, end=" ")
+    print()
